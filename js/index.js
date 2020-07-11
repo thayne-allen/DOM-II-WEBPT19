f04a35f10d42140fb4c33a48c33b3665f4934833
thayne-allen <thayne.allen@gmail.com>
2020-07-10 20:28:34 -0600

    button events

--- js/index.js
+++ js/index.js
@@ -21,3 +21,19 @@ const divClick = document.querySelector('#divBorder');
   divClick.addEventListener('click', function(event){
     event.currentTarget.style.background = 'aqua';
   });
+
+  const button1 = document.querySelector('#btn1');
+  button1.addEventListener('click', function(event){
+    event.target.style.color = 'black';
+    event.stopPropagation();
+  });
+
+const button2 = document.querySelector('#btn2');
+  button2.addEventListener('mouseover', function(event){
+    event.target.style.background = 'black';
+  });
+  
+const button3 = document.querySelector('#btn3');
+  button3.addEventListener('dblclick', function(event){
+    event.target.style.borderColor = 'red';
+  });
