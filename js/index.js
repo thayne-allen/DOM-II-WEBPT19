9e3ef1b1c10479e3a7f97ef3f2bb2930a827f068
thayne-allen <thayne.allen@gmail.com>
2020-07-10 19:54:31 -0600

    click opacity

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@
 window.addEventListener("load", function(event) {
     alert("Fasten your seatbelt, you are about to enter the FunBus Zone!");
     });
+
+    window.addEventListener('mousedown', function(event) {
+        event.target.style.opacity = '0';
+    });
+    
+    window.addEventListener('mouseup', function(event) {
+        event.target.style.opacity = '1';
+    })
